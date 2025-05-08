d63ca1b709963c9a645d8a1bdab4f9dbc9959118
Daniel Medrano Huerta <daniel@quicktortoise.com>
2025-05-07 19:09:10 -0600

    Class 19

---
+++ src/Acme.Ecommerce.Domain.Core/CustomerDomain.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Acme.Ecommerce.Domain.Entity;
+using Acme.Ecommerce.Domain.Interface;
+using Acme.Ecommerce.Infrastructure.Interface;
+
+namespace Acme.Ecommerce.Domain.Core
+{
+    public class CustomerDomain : ICustomerDomain
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerDomain(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public Customer? Get(string customerId)
+        {
+            return _customerRepository.Get(customerId);
+        }
+
+        public async ValueTask<Customer?> GetAsync(string customerId)
+        {
+            return await _customerRepository.GetAsync(customerId);
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return _customerRepository.GetAll();
+        }
+
+        public async ValueTask<IEnumerable<Customer>> GetAllAsync()
+        {
+            return await _customerRepository.GetAllAsync();
+        }
+
+        public bool Insert(Customer customer)
+        {
+            return _customerRepository.Insert(customer);
+        }
+
+        public async ValueTask<bool> InsertAsync(Customer customer)
+        {
+            return await _customerRepository.InsertAsync(customer);
+        }
+
+        public bool Update(Customer customer)
+        {
+            return _customerRepository.Update(customer);
+        }
+
+        public async ValueTask<bool> UpdateAsync(Customer customer)
+        {
+            return await _customerRepository.UpdateAsync(customer);
+        }
+
+        public bool Delete(string customerId)
+        {
+            return _customerRepository.Delete(customerId);
+        }
+
+        public async ValueTask<bool> DeleteAsync(string customerId)
+        {
+            return await _customerRepository.DeleteAsync(customerId);
+        }
+    }
+}
---
+++ src/Acme.Ecommerce.Domain.Interface/ICustomerDomain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Acme.Ecommerce.Domain.Entity;
+
+namespace Acme.Ecommerce.Domain.Interface
+{
+    public interface ICustomerDomain
+    {
+        Customer? Get(string customerId);
+        ValueTask<Customer?> GetAsync(string customerId);
+        IEnumerable<Customer> GetAll();
+        ValueTask<IEnumerable<Customer>> GetAllAsync();
+        bool Insert(Customer customer);
+        ValueTask<bool> InsertAsync(Customer customer);
+        bool Update(Customer customer);
+        ValueTask<bool> UpdateAsync(Customer customer);
+        bool Delete(string customerId);
+        ValueTask<bool> DeleteAsync(string customerId);
+    }
+}
